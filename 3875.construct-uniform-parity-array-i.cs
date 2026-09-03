a53c6a5f294e8e2e8e3c5126e578c98a2137000b
Dmytro Cherepakha <dcherepakha@gmail.com>
2026-09-03 12:39:16 +0200

    grammar

--- 3875.construct-uniform-parity-array-i.cs
+++ 3875.construct-uniform-parity-array-i.cs
@@ -3,12 +3,15 @@
  *
  * [3875] Construct Uniform Parity Array I
  *
- * This problem is pretty ironic.
- * If the initial array contains only even or only odd numbers - the answer is true.
- * If there is at least one odd number - the answer is true, as it is always
- * possible to build an odd number from an even and an odd.
- * If there two and more odd numbers - the answer is true, as it is always
- * possible to build an even number from two odd numbers. 
+ * This problem is pretty ironic, because the answer is always true.
+ * If the initial array contains only even numbers or only odd numbers -
+ * the answer is true.
+ * If there is at least one odd number, the answer is true, as it is always
+ * possible to build an odd number from an even number and an odd number.
+ * If there are two or more odd numbers, the answer is true, as it is always
+ * possible to build an even number from two odd numbers.
+ * And even more: if there is at least one odd number in the array, you could
+ * always make an array of all odd numbers.
  */
 
 // @lc code=start
